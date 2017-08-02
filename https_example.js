54731d7b425d2b976fead7019eb2dd7cd0512cfb
Vadim Gavrish <vadim@gavrish.ca>
2017-08-02 12:11:35 +0000

    added response callback

--- https_example.js
+++ https_example.js
@@ -6,8 +6,9 @@ var options = {
 };
 
 
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+  console.log('Response :', response);
 }
 
 console.log("I'm about to make the request!");
